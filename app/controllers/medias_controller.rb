7c9dee4e9ec8563e2fb5d0b0f1ed9bef1b5a1aec
Olivia <reach.olivialegge@gmail.com>
2016-10-11 13:44:51 -0700

    added models, controlers, and resources for routes

---
+++ app/controllers/medias_controller.rb
@@ -0,0 +1,4 @@
+class MediasController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+
+  get 'medias/index'
+
+  Rails.application.routes.draw do
+  resources :movies
+  end
+
+  Rails.application.routes.draw do
+  resources :books
+  end
+
+  Rails.application.routes.draw do
+  resources :albums
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
