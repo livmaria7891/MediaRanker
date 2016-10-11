a693e3068d8adcdbe006651be491fa63b9c807f6
Olivia <reach.olivialegge@gmail.com>
2016-10-11 14:25:24 -0700

    added ability to create new movie and list on movie index page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,25 @@
 class MoviesController < ApplicationController
   def index
+    @movie = Movie.all
   end
 
   def show
   end
 
   def new
+    @movie = Movie.new
   end
 
   def create
+    @params = params
+    #and why is this here?
+    @movie = Movie.new
+    @movie.title = params[:movie][:title]
+    @movie.director = params[:movie][:director]
+    @movie.description = params[:movie][:description]
+    @movie.rank = 0
+    @movie.save
+    # redirect_to action: 'index'
   end
 
   def edit
